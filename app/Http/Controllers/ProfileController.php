c61d8a5fa87057308015f12285edc91d9259bb2e
RCRalph <r.chrzanowski.priv@gmail.com>
2021-02-25 10:39:00 +0100

    Fixed route names and added cache forgetting after profile picture change

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cache;
 
 class ProfileController extends Controller
 {
