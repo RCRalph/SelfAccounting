3f88447a0412d1752e8f23a13fa8c4183e249b6f
RCRalph <rcralph@rcralph.me>
2022-09-03 20:06:26 +0200

    Added pagination to report view

--- resources/js/components/extensions/reports/ViewReportComponent.vue
+++ resources/js/components/extensions/reports/ViewReportComponent.vue
@@ -169,6 +169,12 @@
                     </div>
                 </v-col>
             </v-row>
+
+            <v-pagination
+                class="mt-3"
+                v-model="currentReport"
+                :length="reports.length"
+            ></v-pagination>
         </div>
 
         <v-overlay v-else :value="true" opacity="1" absolute>
@@ -212,6 +218,8 @@ export default {
                 date: []
             },
             lastChange: new Date(),
+            reports: [],
+            currentReport: null,
 
             ready: false,
             success: false,
@@ -276,6 +284,8 @@ export default {
 
                     this.information = data.information;
                     this.content = data.items;
+                    this.reports = data.reports;
+                    this.currentReport = this.reports.indexOf(Number(this.$route.params.id)) + 1;
 
                     this.ready = true;
                 })
