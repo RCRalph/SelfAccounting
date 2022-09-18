c5db13e876ed70b27e0eee111e9842bd763c495b
RCRalph <rcralph@rcralph.me>
2022-09-18 23:20:01 +0200

    Moved Models to its own folder, deleted old validation rules, added payment for premium routes and methods

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-use App\Extension;
+use App\Models\Extension;
 
 class PaymentController extends Controller
 {
@@ -36,6 +36,32 @@ class PaymentController extends Controller
             }
 
             return redirect("/app#/extensions/$extension->directory");
+        }
+
+        return redirect()->route("payment.failure");
+    }
+
+    public function premium($length)
+    {
+        if ($length == 1) {
+            return auth()->user()
+                ->newSubscription("premium", "price_1LiOD9AmJbobJ1Gs4sXJhXvJ")
+                ->checkout([
+                    "success_url" => route("payment.premium.success", $length) . "?session_id={CHECKOUT_SESSION_ID}",
+                    "cancel_url" => route("payment.failure")
+                ]);
+        }
+        else if ($length == 12) {
+
+        }
+
+        return redirect()->route("payment.failure");
+    }
+
+    public function premiumSuccess()
+    {
+        if (request("session_id") && auth()->user()->stripe()->checkout->sessions->retrieve(request("session_id"))->payment_status == "paid") {
+            return redirect("/app#/profile");
         }
 
         return redirect()->route("payment.failure");
