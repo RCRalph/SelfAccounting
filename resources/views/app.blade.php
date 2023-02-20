5e91ba69132263cbd27fefc444881c9ffd27a1d4
RCRalph <r.chrzanowski.priv@gmail.com>
2023-02-20 23:58:39 +0100

    Added full icon set

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" href="{{ URL::asset("favicon.svg") }}" type="image/svg+xml"/>
-
-        <!-- Styles -->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    </head>
+    @include("layouts.head")
 
     <body>
         <div id="app">
