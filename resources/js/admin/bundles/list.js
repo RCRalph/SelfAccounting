c1dc49a773f61365f370addd533a18c2c05a7cd5
RCRalph <rcralph@rcralph.me>
2024-02-28 20:18:14 +0100

    Removed Vue 2 + Vuetify 2 client version

--- resources/js/admin/bundles/list.js
+++
@@ -1,6 +0,0 @@
-Vue.component('bundle-list-component', require('./components/BundleListComponent.vue').default);
-Vue.component('pagination', require('laravel-vue-pagination'));
-
-const app = new Vue({
-    el: '#app'
-});
--- resources/js/admin/users/details.js
+++
@@ -1,5 +0,0 @@
-Vue.component('user-details-component', require('./components/UserDetailsComponent.vue').default);
-
-const app = new Vue({
-    el: '#app'
-});
