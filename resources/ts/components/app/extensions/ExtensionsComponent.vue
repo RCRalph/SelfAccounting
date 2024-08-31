9fc88bce1b373f179e6fe46b7aa4a5c00d8bdf9e
RCRalph <rcralph@rcralph.me>
2024-08-31 12:20:47 +0200

    Fixed issue with invalid tab being selected after refresh

--- resources/ts/components/app/extensions/ExtensionsComponent.vue
+++ resources/ts/components/app/extensions/ExtensionsComponent.vue
@@ -1,24 +1,17 @@
 <template>
     <div class="tabs">
         <v-tabs
+            v-model="currentTab"
             align-tabs="center"
             center-active
             show-arrows
             stacked
         >
             <v-tab
-                value="store"
-                prepend-icon="mdi-shopping"
-                to="/extensions/store"
-            >
-                Store
-            </v-tab>
-
-            <v-tab
-                v-for="item in extensions.enabledExtensions"
-                :value="item.code"
+                v-for="item in tabs"
+                :value="item.value"
                 :prepend-icon="item.icon"
-                :to="`/extensions/${item.directory}`"
+                :to="item.to"
             >
                 {{ item.title }}
             </v-tab>
@@ -29,7 +22,36 @@
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted, ref } from "vue"
 import { useExtensionsStore } from "@stores/extensions"
+import { useRoute } from "vue-router"
 
 const extensions = useExtensionsStore()
+const route = useRoute()
+
+const tabs = computed(() => [
+    {
+        value: "store",
+        icon: "mdi-shopping",
+        to: "/extensions/store",
+        title: "Store",
+    },
+    ...extensions.enabledExtensions.map(item => ({
+        value: item.code,
+        icon: item.icon,
+        to: `/extensions/${item.directory}`,
+        title: item.title,
+    })),
+])
+
+const currentTab = ref("store")
+
+onMounted(() => {
+    for (const item of tabs.value) {
+        if (route.path.startsWith(item.to)) {
+            currentTab.value = item.value
+            break
+        }
+    }
+})
 </script>
